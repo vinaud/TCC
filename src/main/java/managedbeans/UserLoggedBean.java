a510c7bfa609a6328ef4377086e7bc6a42e856bd
João Vinaud <leyenasd@gmail.com>
2018-09-21 14:33:19 -0300

    Ajuste na barra de experiencia

--- src/main/java/managedbeans/UserLoggedBean.java
+++ src/main/java/managedbeans/UserLoggedBean.java
@@ -6,19 +6,25 @@ import persistence.UserDAOHibernate;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import java.io.Serializable;
 
 
 @ManagedBean
-@SessionScoped
+@ViewScoped
 public class UserLoggedBean implements Serializable {
     UserDAOHibernate dao = new UserDAOHibernate();
     User logado;
+    int nextxp=0;
+    int reason=0;
 
     @PostConstruct
     public void login()
     {
         logado = dao.getUsuario(56);
+        nextxp = logado.getLevel()*110;
+        reason = Math.round((logado.getExp()*100)/nextxp );
+       // reason = 60;
     }
 
 
@@ -28,5 +34,21 @@ public class UserLoggedBean implements Serializable {
 
     public void setLogado(User logado) {
         this.logado = logado;
+    }
+
+    public int getNextxp() {
+        return nextxp;
+    }
+
+    public void setNextxp(int nextxp) {
+        this.nextxp = nextxp;
+    }
+
+    public int getReason() {
+        return reason;
+    }
+
+    public void setReason(int reason) {
+        this.reason = reason;
     }
 }
