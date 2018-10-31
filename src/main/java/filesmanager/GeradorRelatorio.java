d05faca74f92ff0d62ae3a3d2856d5e5977cad89
João Vinaud <leyenasd@gmail.com>
2018-10-31 16:13:37 -0300

    Criada classe para geração de relatorios (nao implementada ainda)

---
+++ src/main/java/filesmanager/GeradorRelatorio.java
@@ -0,0 +1,18 @@
+package filesmanager;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+public class GeradorRelatorio {
+
+    public static  void writeStream(String relatorio) throws IOException {
+        Writer writer = new FileWriter("C:\\Users\\João Vinaud\\Desktop\\outputOne.txt");
+
+        try {
+            writer.write(relatorio);
+        } finally {
+            writer.close();
+        }
+    }
+}
